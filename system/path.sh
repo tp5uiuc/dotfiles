d7e4c4dc49ba1c70f70a0de2ccbc3b8fbe52bc4a
Tejaswin Parthasarathy <tp5@illinois.edu>
2019-09-02 13:26:25 -0500

    fix : paths, changed MBP to localrc

--- system/path.sh
+++ system/path.sh
@@ -1,7 +1,5 @@
-#!/bin/sh
-export PATH="$PATH:$DOTFILES/bin:$HOME/.bin"
-
-export PATH="/usr/local/bin:$PATH:/usr/local/sbin:$(brew --prefix coreutils)/libexec/gnubin:/Users/tp5/Desktop/ursolver/pythonSource/utils:/Applications/ParaView-5.5.0.app/Contents/bin:/Applications/MATLAB_R2016b.app/bin";
+#!usr/bin/env sh
+export PATH="/usr/local/bin:$PATH:$DOTFILES/bin:$HOME/.bin"
 
 # your project folder that we can `c [tab]` to
 export PROJECTS="$HOME/code"
@@ -9,15 +7,6 @@ export PROJECTS="$HOME/code"
 # your default editor
 export EDITOR='nvim'
 export VEDITOR='subl'
-
-# Added to make finding VTK,TBB easier when using CMake
-# Hardcoded as brew is observed to be darn slow when using
-# $(brew --prefix)
-if command -v brew >/dev/null 2>&1; then
-  export TBB_ROOT=/usr/local/opt/tbb
-  export VTK_ROOT=/usr/local/opt/vtk
-  export FFTW_ROOT=/usr/local/opt/fftw
-fi
 
 # Make Python use UTF-8 encoding for output to stdin, stdout, and stderr.
 export PYTHONIOENCODING='UTF-8';
