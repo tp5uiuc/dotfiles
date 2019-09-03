e26cdc6dd1dc91fc3a407f7e87cb078366baff8e
Tejaswin Parthasarathy <tp5@illinois.edu>
2019-09-02 23:43:10 -0500

    fix : antibody bundles, permissions and shebang
    No permissions + wrong shebang given to install

--- antibody/install.sh
+++ antibody/install.sh
@@ -1,6 +1,6 @@
-#!usr/bin/env sh
+#!/usr/bin/env sh
 
 if command -v antibody >/dev/null 2>&1; then
-  antibody bundle <"$DOTFILES/antibody/bundles.txt" >~/.zsh_plugins.sh
-  antibody update
+	antibody bundle <"$DOTFILES/antibody/bundles.txt" >~/.zsh_plugins.sh
+	antibody update
 fi
