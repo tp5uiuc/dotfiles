b37a4a6cfd35000ead5a1d1adbd56fa85708591d
Tejaswin Parthasarathy <tp5@illinois.edu>
2019-09-03 14:18:29 -0500

    feat : add z to bash as a separate script

---
+++ bash/external.bash
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Manage/source external bash scripts
+
+# z!
+ANTIBODY_Z_FILEPATH="$HOME/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-rupa-SLASH-z/z.sh"
+
+BREW_Z_FILEPATH="/usr/local/etc/profile.d/z.sh"
+
+if [[ -d "$(dirname ${ANTIBODY_Z_FILEPATH})" && -f "${ANTIBODY_Z_FILEPATH}" ]]; then
+	. ${ANTIBODY_Z_FILEPATH}
+elif [[ -r "${BREW_Z_FILEPATH}" && -f "${BREW_Z_FILEPATH}" ]]; then
+	. ${BREW_Z_FILEPATH}
+fi
