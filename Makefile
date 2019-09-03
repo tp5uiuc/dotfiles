5c985b7ac9ebb42e9d43d2b4e7b438ca982e2504
Tejaswin Parthasarathy <tp5@illinois.edu>
2019-09-03 14:19:02 -0500

    fix : shformatting for bash files

--- Makefile
+++ Makefile
@@ -1,7 +1,8 @@
+# The last regexp in find matches bash and zsh files
 fmt:
 	@shfmt --version
 	@find . -maxdepth 3 -name '*.sh' | while read -r src; do shfmt -w -p "$$src"; done
-	@find . -maxdepth 3 -name '*.zsh' | while read -r src; do shfmt -w "$$src"; done;
+	@find . -maxdepth 3 -name '*.[z|b]**sh' | while read -r src; do shfmt -w "$$src"; done;
 
 check:
 	@curl -sL https://git.io/shcheck | bash -f -s -- \
--- bash/config.bash
+++ bash/config.bash
@@ -1,26 +1,26 @@
 #!/usr/bin/env bash
 
 # Case-insensitive globbing (used in pathname expansion)
-shopt -s nocaseglob;
+shopt -s nocaseglob
 
 # Turn on extended globbing options (one or more match etc.)
-shopt -s extglob;
+shopt -s extglob
 
 # Append to the Bash history file, rather than overwriting it
-shopt -s histappend;
+shopt -s histappend
 
 # Autocorrect typos in path names when using `cd`
-shopt -s cdspell;
+shopt -s cdspell
 
 # Enable some Bash 4 features when possible:
 # * `autocd`, e.g. `**/qux` will enter `./foo/bar/baz/qux`
 # * Recursive globbing, e.g. `echo **/*.txt`
 for option in autocd globstar; do
-  shopt -s "$option" 2> /dev/null;
-done;
+	shopt -s "$option" 2>/dev/null
+done
 
 # Increase Bash history size. Allow 32³ entries; the default is 500.
-export HISTSIZE='32768';
-export HISTFILESIZE="${HISTSIZE}";
+export HISTSIZE='32768'
+export HISTFILESIZE="${HISTSIZE}"
 # Omit duplicates and commands that begin with a space from history.
-export HISTCONTROL='ignoreboth';
+export HISTCONTROL='ignoreboth'
