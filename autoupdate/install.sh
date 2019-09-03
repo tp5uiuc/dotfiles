c1da6f41d4a2c660868cdd4137134c03182d97bf
Tejaswin Parthasarathy <tp5@illinois.edu>
2019-09-03 13:01:05 -0500

    feat : crontab for autoupdating dotfiles

---
+++ autoupdate/install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+# setups the auto-update
+(
+  crontab -l | grep -v "dot_update"
+	#=====================================================================#
+	#     0-23   0-59               0-30  0-6   1-12                      #
+	#     |_    _|                 _|    _|    _|                         #
+	#       |  |                  |     |     |            * any          #
+	#    At HH:MM on day-of-month MD on WD in M            - range        #
+	#       |  |                  |     |     |            , array        #
+	#     __|__|            ______|     |   __|            / step         #
+	#    |  |            __|         ___|__|                              #
+	#   _|  |__         |         __|   |__                               #
+	#  |       |        |        |         |                              #
+	#  |       |        |        |         |                              #
+	#=====================================================================#
+	#  Min  |  Hour  |  Mday  |  Month  |  Wday  |  Command               #
+	#=====================================================================#
+  echo "0 0 * * FRI $HOME/.dotfiles/bin/dot_update > ${TMPDIR:-/tmp}/dot_update.log 2>&1"
+) | crontab -
