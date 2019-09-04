0283037846f82ebd66f1b05c2f697f7a9d1d530a
Tejaswin Parthasarathy <tp5@illinois.edu>
2019-09-04 17:20:32 -0500

    feat : hosts file for systemwide adblocking

---
+++ hosts/install.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+
+download_hosts() {
+	BLACK_URL="https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn/hosts"
+	TOPSIFY_URL="https://raw.githubusercontent.com/zackad/dotfiles/master/hosts.d/ads_spotify.conf"
+	mkdir -p /tmp/hosts.d/
+	curl -L -s -o /tmp/hosts.d/steven_black.host "$BLACK_URL"
+	curl -L -s -o /tmp/hosts.d/topsify.host "$TOPSIFY_URL"
+
+	unset BLACK_URL TOPSIFY_URL
+}
+
+cat_hosts() {
+	cat /tmp/hosts.d/*.host >/tmp/hosts.d/master.hosts
+	echo '#DOTFILE_HOSTS#' | cat - /tmp/hosts.d/master.hosts >/etc/hosts && rm -fr /tmp/hosts.d/master.hosts
+}
+
+if ( (EUID)); then
+	# If dotfiles, don't create a backup
+	test "$(head -n1 /etc/hosts)" = '#DOTFILE_HOSTS#' || mv /etc/hosts /etc/hosts.bkup
+	download_hosts
+	cat_hosts
+else
+	echo "Please run hosts as root"
+	exit 1
+fi
+unset download_hosts cat_hosts
