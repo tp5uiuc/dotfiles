6bb759972dfe908103f5e5a347ffb9d1462b1003
Tejaswin Parthasarathy <tp5@illinois.edu>
2019-09-03 23:59:45 -0500

    feat : awesome fzf

---
+++ fzf/completion.bash
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+test -d /usr/local/opt/fzf/shell || return 0
+
+export FZF_COMPLETION_TRIGGER='**'
+
+# shellcheck disable=SC1091,SC1094
+. /usr/local/opt/fzf/shell/completion.bash
