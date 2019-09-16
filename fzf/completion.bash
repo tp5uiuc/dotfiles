1cfa2b5dca32c10d6d224cbb66be9b5579f6ab44
Tejaswin Parthasarathy <tp5@illinois.edu>
2019-09-16 00:14:50 -0500

    docs : added comments for bash keybindings in fzf

--- fzf/completion.bash
+++ fzf/completion.bash
@@ -7,4 +7,4 @@ export FZF_COMPLETION_TRIGGER='**'
 # shellcheck disable=SC1091,SC1094
 . /usr/local/opt/fzf/shell/completion.bash
 # shellcheck disable=SC1091,SC1094
-. /usr/local/opt/fzf/shell/key-bindings.bash
+. /usr/local/opt/fzf/shell/key-bindings.bash # Note that we already source it in config.zsh, but to maintain symmetry with the bash script we do it here too.
