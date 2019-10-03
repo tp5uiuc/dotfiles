d58e791abcb373b6474100d7aeb1bb9b70b32d7a
Tejaswin Parthasarathy <tp5@illinois.edu>
2019-10-03 02:56:02 -0500

    fix : install antibody only on CI environments

--- antibody/install.sh
+++ antibody/install.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env sh
 
+# Build antibody only in CI environments
+test -n "$CI" &&
+	curl -sL https://git.io/antibody | sh -s
+
 if command -v antibody >/dev/null 2>&1; then
 	antibody bundle <"$DOTFILES/antibody/bundles.txt" >~/.zsh_plugins.sh
 	antibody update
